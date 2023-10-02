c45ca8ed25a73cd42a323f8217b484dbffedec0d
jordy Eulink <jordy.eulink31@gmail.com>
2023-10-02 14:35:54 +0200

    afronding exit scherm

--- src/main/java/com/github/hanyaeger/tutorial/scenes/EndScene.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/EndScene.java
@@ -6,6 +6,7 @@ import com.github.hanyaeger.api.entities.impl.TextEntity;
 import com.github.hanyaeger.api.scenes.StaticScene;
 import com.github.hanyaeger.api.userinput.MouseButtonPressedListener;
 import com.github.hanyaeger.tutorial.WaterWorld;
+import com.github.hanyaeger.tutorial.entities.buttons.ExitButton;
 import com.github.hanyaeger.tutorial.entities.buttons.StartButton;
 import javafx.scene.input.MouseButton;
 import javafx.scene.paint.Color;
@@ -30,11 +31,18 @@ public class EndScene extends StaticScene implements MouseButtonPressedListener 
     public void setupEntities() {
     var PlayAgain = new StartButton(new Coordinate2D(getWidth() / 2, getHeight() / 2),waterWorld);
 
+    var Exit = new ExitButton(new Coordinate2D(getWidth() / 2, getHeight() / 2 + 100));
+
     PlayAgain.setAnchorPoint(AnchorPoint.CENTER_CENTER);
     PlayAgain.setFill(Color.DARKBLUE);
     PlayAgain.setFont(Font.font("Roboto", FontWeight.SEMI_BOLD, 80));
     addEntity(PlayAgain);
-    }
+
+        Exit.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        Exit.setFill(Color.DARKBLUE);
+        Exit.setFont(Font.font("Roboto", FontWeight.SEMI_BOLD, 80));
+    addEntity(Exit);
+}
 
     @Override
     public void onMouseButtonPressed(MouseButton mouseButton, Coordinate2D coordinate2D) {
