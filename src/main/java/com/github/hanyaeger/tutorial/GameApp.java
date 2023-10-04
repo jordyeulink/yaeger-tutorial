dae535cce167bfc62a7d7802126d532525ffcdae
jordy Eulink <jordy.eulink31@gmail.com>
2023-10-04 10:04:29 +0200

    meer basis

--- src/main/java/com/github/hanyaeger/tutorial/GameApp.java
+++ src/main/java/com/github/hanyaeger/tutorial/GameApp.java
@@ -1,4 +1,27 @@
 package com.github.hanyaeger.tutorial;
 
-public class GameApp {
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.YaegerGame;
+import com.github.hanyaeger.tutorial.scenes.EndScene;
+import com.github.hanyaeger.tutorial.scenes.GameLevel;
+import com.github.hanyaeger.tutorial.scenes.TitleScene;
+
+public class GameApp extends YaegerGame {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void setupGame() {
+        setGameTitle("Star Shooter");
+        setSize(new Size(800, 600));
+    }
+
+
+    @Override
+    public void setupScenes(){
+
+
+}
 }
