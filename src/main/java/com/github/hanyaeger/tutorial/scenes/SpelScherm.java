38e069894b20160d8280574b9330775d6adf9fa9
jordy Eulink <jordy.eulink31@gmail.com>
2023-11-01 13:17:56 +0100

    polymorfie toepassing

--- src/main/java/com/github/hanyaeger/tutorial/scenes/SpelScherm.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/SpelScherm.java
@@ -11,10 +11,12 @@ import com.github.hanyaeger.tutorial.entities.Meteorieten;
 import com.github.hanyaeger.tutorial.entities.karakters.Speeder;
 import com.github.hanyaeger.tutorial.entities.karakters.Speler;
 import com.github.hanyaeger.tutorial.entities.karakters.Tank;
+import com.github.hanyaeger.tutorial.entities.karakters.Tegenstander;
 import com.github.hanyaeger.tutorial.entities.text.HealthText;
 import com.github.hanyaeger.tutorial.timers.TegenstanderTimer;
 import javafx.scene.input.MouseButton;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 
@@ -22,6 +24,7 @@ import java.util.Random;
 public class SpelScherm extends DynamicScene implements MouseButtonPressedListener,TimerContainer{
     private TegenstanderTimer timer;
 
+    ArrayList <Tegenstander> lijst = new ArrayList<Tegenstander>();
     private GameApp gameApp;
     private HealthText Levens = new HealthText(new Coordinate2D(200,10));
     private Tank tank;
@@ -46,6 +49,8 @@ public class SpelScherm extends DynamicScene implements MouseButtonPressedListen
         tank = new Tank(new Coordinate2D(getWidth()-100, 100), new Size(50,50), this);
         speeder = new Speeder(new Coordinate2D(getWidth()-100, 100), new Size(50,50), this);
         timer = new TegenstanderTimer(1000, tank, speeder, this);
+        lijst.add(tank);
+        lijst.add(speeder);
 
         Random rand = new Random();
 
@@ -53,11 +58,12 @@ public class SpelScherm extends DynamicScene implements MouseButtonPressedListen
             meteoriet = new Meteorieten(500 * rand.nextDouble(),500 * rand.nextDouble());
             addEntity(meteoriet);
         }
+        for(Tegenstander tegenstander: lijst){
+            addEntity(tegenstander);
+        }
 
         addEntity(speler);
         addEntity(Levens);
-        addEntity(tank);
-        addEntity(speeder);
     }
 
     public void entityToevoegen(YaegerEntity entity) {
